fa7a34d5d45590d277e86f9d1ea20b121ffdfbcc
gabri3lquadr0s <gabrielquadrosrodapiao@gmail.com>
2024-05-02 11:21:59 -0300

    auth, swagger, and user ong modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
+import { OngsModule } from './modules/ongs/ongs.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, OngsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('API - Compasio')
+    .setDescription('APIs do Projeto da S.A. Compasio')
+    .setContact('Compasio', 'https://github.com/Compasio', '')
+    .setVersion('0.1')
+    .addTag('Auth', 'Autenticação do Sistema')
+    .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
