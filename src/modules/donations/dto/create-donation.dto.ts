c4aa182d07349b2e2c6b77a77dd23f2b8478e92f
gabri3lquadr0s <gabrielquadrosrodapiao@gmail.com>
2024-05-23 09:45:41 -0300

    add donation dto

--- src/modules/donations/dto/create-donation.dto.ts
+++ src/modules/donations/dto/create-donation.dto.ts
@@ -1,1 +1,31 @@
-export class CreateDonationDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Permissions } from '@prisma/client';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+  IsEmail,
+  IsJSON,
+  ValidateNested,
+  IsDate,
+  IsDateString,
+  IsUrl,
+  isNumber,
+  isNotEmpty,
+  IsArray
+} from 'class-validator';
+
+export class CreateDonationDto {
+    @ApiProperty({type: Number, description: "ID Do voluntario", example: 1})
+    @IsNotEmpty()
+    @IsNumber()
+    voluntario: number
+
+    @ApiProperty({type: Number, description: "Vaquinha",example: 1})
+    @IsOptional()
+    @IsNumber()
+    crowdfunding : number
+}
