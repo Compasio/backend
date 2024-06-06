9e428aacb26e0eee54c755ca7e822128b84bbab3
gabri3lquadr0s <gabrielquadrosrodapiao@gmail.com>
2024-06-06 12:22:20 -0300

    fix api and getAll

--- src/modules/maps/maps.service.ts
+++ src/modules/maps/maps.service.ts
@@ -23,13 +23,11 @@ export class MapsService {
     });
     if(!userExists) throw new NotFoundException("Usuário não encontrado");
 
-    //const address = `${createMapDto.num}%20${createMapDto.street.replace(/\s+/g, '%20')}%20${createMapDto.neighborhood.replace(/\s+/g, '%20')}%20${createMapDto.city.replace(/\s+/g, '%20')}%20${createMapDto.state}`;
-    let address = "Los%20Angeles"
-    console.log(`https://api.mapbox.com/search/geocode/v6/forward?q=${address}&access_token=${process.env.MAP}`)
-    console.log(address);
+    let address = `${createMapDto.num} ${createMapDto.street} ${createMapDto.neighborhood} ${createMapDto.city} ${createMapDto.state}`.replace(/\s+/g, '%20');
+    let url = `https://api.mapbox.com/search/geocode/v6/forward?q=${address}&access_token=${process.env.MAP}`;
 
     const { data } = await firstValueFrom(
-      this.httpService.get(`https://api.mapbox.com/search/geocode/v6/forward?q=${address}&access_token=${process.env.MAP}&autocomplete=true&country=BR`).pipe(
+        this.httpService.get(url).pipe(
         catchError((error: AxiosError) => {
           this.logger.error(error.response.data);
           throw 'Erro';
@@ -37,15 +35,38 @@ export class MapsService {
       )
     )
 
-    console.log(data);
-    for(let i = 0; i < 5; i++) {
-      console.log(data.features[i].geometry)
-    }
+
+    return this.prisma.address.create({
+      data: {
+        id_user: id,
+        lat: data.features[0].geometry.coordinates[1],
+        lng: data.features[0].geometry.coordinates[0]
+      }
+    })
     
   }
 
-  findAll() {
-    return `This action returns all maps`;
+  async getAllAddress( page: number) {
+    let requests;
+    let count = await this.prisma.address.count({})
+
+    if (page == 0){
+      requests = await this.prisma.address.findMany({})
+    }
+    
+    else if(page == 1){
+      requests = await this.prisma.address.findMany({
+
+      });
+    }
+    else{
+      requests = await this.prisma.address.findMany({
+        take: 20,
+        skip: (page - 1) * 20,
+      });
+    }
+    if(requests[0] == undefined) return[];
+    return{"requests": requests, "totalCount": count};
   }
 
   findOne(id: number) {
