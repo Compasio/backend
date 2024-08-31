c34e5d156b0d9ff4ae9e05915f2af2ea9d205b71
gabri3lquadr0s <gabrielteodoto0710@gmail.com>
2024-08-31 17:26:34 -0300

    MapModule last routes

--- src/modules/maps/dto/update-map.dto.ts
+++ src/modules/maps/dto/update-map.dto.ts
@@ -1,4 +1,32 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateMapDto } from './create-map.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { State } from '@prisma/client';
 
-export class UpdateMapDto extends PartialType(CreateMapDto) {}
+export class UpdateMapDto {
+  @ApiProperty({type: String, description: "Numero", example: "36"})
+  @IsString()
+  @IsNotEmpty()
+  num: string;
+
+  @ApiProperty({type: String, description: "Rua, avenida, etc", example: "Avenida Paulista"})
+  @IsString()
+  @IsNotEmpty()
+  street: string;
+
+  @ApiProperty({type: String, description: "Bairro", example: "Bela Vista"})
+  @IsString()
+  @IsNotEmpty()
+  neighborhood: string;
+
+  @ApiProperty({type: String, description: "Cidade", example: "São Paulo"})
+  @IsString()
+  @IsNotEmpty()
+  city: string;
+
+  @ApiProperty({type: String, description: "Bairro", example: "SP"})
+  @IsString()
+  @IsNotEmpty()
+  state: State;
+}
--- src/modules/maps/maps.controller.ts
+++ src/modules/maps/maps.controller.ts
@@ -53,23 +53,62 @@ export class MapsController {
   @ApiOkResponse({description: 'Requisição feita com sucesso', status: 201})
   @ApiBadRequestResponse({ description: 'Requisição inválida', status: 400})
   @ApiParam({ name: 'ongname', schema: { default: "example" } })
-  @ApiOperation({summary: 'Retorna uma lista de vinte voluntários por página'})
+  @ApiOperation({summary: 'Retorna lista de endereçõs com ongs que correspondem ao input'})
   async getAddressFromOng(@Param('ongname') ongname: string) {
     return this.mapsService.getAddressFromOng(ongname);
   }
 
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.mapsService.findOne(+id);
-  // }
+  @Public()
+  @Get('getOngsByPlace/:place')
+  @ApiOkResponse({description: 'Requisição feita com sucesso', status: 201})
+  @ApiBadRequestResponse({ description: 'Requisição inválida', status: 400})
+  @ApiParam({ name: 'place', schema: { default: "36 Avenida Paulista" } })
+  @ApiOperation({summary: 'Retorna uma lista de 30 ongs correspondentes'})
+  async getOngsByPlace(@Param('place') place: string) {
+    return this.mapsService.getOngsByPlace(place);
+  }
 
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateMapDto: UpdateMapDto) {
-  //   return this.mapsService.update(+id, updateMapDto);
-  // }
+  @Public()
+  @Get('getNearestOgns/:userLat/:userLng/:radius')
+  @ApiOkResponse({description: 'Requisição feita com sucesso', status: 201})
+  @ApiBadRequestResponse({ description: 'Requisição inválida', status: 400})
+  @ApiParam({ name: 'userLat', schema: { default: -23.571101 } })
+  @ApiParam({ name: 'userLng', schema: { default: -46.644646 } })
+  @ApiParam({ name: 'radius', schema: { default: 10 } })
+  @ApiOperation({summary: 'Retorna uma lista de 30 ongs correspondentes'})
+  async getNearestOgns(@Param('userLat') userLat: number, @Param('userLng') userLng: number, @Param('radius') radius: number) {
+    return this.mapsService.getNearestOgns(userLat, userLng, radius);
+  }
 
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.mapsService.remove(+id);
-  // }
+  @UserTypeAuth('admin', 'voluntary', 'ong', 'ongAssociated')
+  @Get('getAddressById/:id')
+  @ApiOkResponse({description: 'Requisição feita com sucesso', status: 201})
+  @ApiBadRequestResponse({ description: 'Requisição inválida', status: 400})
+  @ApiParam({ name: 'id', schema: { default: 1 } })
+  @ApiOperation({summary: 'Retorna um endereço pelo id do usuário'})
+  async getAddressById(@Param('id') id: number) {
+    return await this.mapsService.getAddressById(+id);
+  }
+
+  @UserTypeAuth('admin', 'ong')
+  @Patch('updateAddress/:id')
+  @ApiOkResponse({description: 'Requisição feita com sucesso', type: UpdateMapDto, status: 201})
+  @ApiBadRequestResponse({ description: 'Requisição inválida', status: 400})
+  @ApiParam({ name: 'id', schema: { default: 1 } })
+  @ApiOperation({summary: 'Atualiza informações do endereço com o respectivo id'})
+  async updatevoluntary(@Param('id') id: number, @Body() updatemapDto: UpdateMapDto, @Request() req) {
+    let confirmPass = await this.authService.checkIdAndAdminStatus(id, req);
+    return await this.mapsService.updateAddress(id, updatemapDto);
+  }
+
+  @UserTypeAuth('admin', 'ong')
+  @Delete('deleteAddress/:id')
+  @ApiOkResponse({description: 'Requisição feita com sucesso', status: 201})
+  @ApiBadRequestResponse({ description: 'Requisição inválida', status: 400})
+  @ApiParam({ name: 'id', schema: { default: 1 } })
+  @ApiOperation({summary: 'Remove endereço com o respectivo id'})
+  async deleteAddress(@Param('id') id: number, @Request() req) {
+    let confirmPass = await this.authService.checkIdAndAdminStatus(id, req);
+    return await this.mapsService.deleteAddress(+id);
+  }
 }
